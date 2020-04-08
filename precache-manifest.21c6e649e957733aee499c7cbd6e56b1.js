1b993ec35957081807480dc199c78d602628faca
adarshsuresh07 <adarshsuresh@cet.ac.in>
2020-04-08 13:51:04 +0530

    Updates

---
+++ precache-manifest.21c6e649e957733aee499c7cbd6e56b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34173491b6291ad0c256f3b254c63bc3",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "d039a0c2c1ce98e4cccb",
+    "url": "/Portfolio/static/css/main.672d112c.chunk.css"
+  },
+  {
+    "revision": "8d0f0e153170e0af6fbd",
+    "url": "/Portfolio/static/js/2.d2bc6d74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.d2bc6d74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d039a0c2c1ce98e4cccb",
+    "url": "/Portfolio/static/js/main.270c0791.chunk.js"
+  },
+  {
+    "revision": "43f1dfbd388c45f57593",
+    "url": "/Portfolio/static/js/runtime-main.2445a1df.js"
+  },
+  {
+    "revision": "0f656f89b0a5da61707fa88bd1ffd16e",
+    "url": "/Portfolio/static/media/football.0f656f89.png"
+  },
+  {
+    "revision": "d0fc0b22109cdb9536fbd54bfe9a30ed",
+    "url": "/Portfolio/static/media/proimg.d0fc0b22.jpg"
+  }
+]);
