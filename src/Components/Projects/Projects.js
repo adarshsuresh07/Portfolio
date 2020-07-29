c491c69723e70a27e3d78f4c44b04d021bd29824
adarshsuresh07 <adarshsuresh@cet.ac.in>
2020-07-29 15:24:13 +0530

    dark mode #3

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -10,6 +10,14 @@ class Education extends React.Component {
         return (
             <div>
                 <div className="row">
+                    <span id="ip3"><FontAwesomeIcon icon={faLightbulb} /></span>
+                    <div>
+                        <h4>Notified</h4>
+                        <a href="https://github.com/adarshsuresh07/Notified" target="_blank">A colaborative platform for job seeking students</a>
+                        <a href="https://be-notified.herokuapp.com/" target="_blank">Try it out!</a>
+                    </div>
+                </div>
+                <div className="row">
                     <span id="ip1"><FontAwesomeIcon icon={faLightbulb} /></span>
                     <div>
                         <h4>Online Fee Payment</h4>
@@ -21,13 +29,6 @@ class Education extends React.Component {
                     <div>
                         <h4>A CGPA Calculator For KTU students</h4>
                         <a href="https://github.com/adarshsuresh07/CGPA-Calculator" target="_blank">CGPA calculator using PyQT</a>
-                    </div>
-                </div>
-                <div className="row">
-                    <span id="ip3"><FontAwesomeIcon icon={faLightbulb} /></span>
-                    <div>
-                        <h4>A book Bucket List</h4>
-                        <a href="https://github.com/adarshsuresh07/Book-Bucket-List" target="_blank">A website which helps you to add a book or remove one with a login.</a>
                     </div>
                 </div>
                 <div id="more">
